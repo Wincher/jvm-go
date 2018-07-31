7977177368849e5ad59cdabe5fa2a18d363709e2
Wincher <wincher.hoo@gmail.com>
2018-08-01 00:37:02 +0800

    feat array and string

---
+++ ch08/instructions/references/new.go
@@ -0,0 +1,30 @@
+package references
+
+import (
+	"jvm_go/ch08/rtda/heap"
+	"jvm_go/ch08/instructions/base"
+	"jvm_go/ch08/rtda"
+)
+
+// Create new object
+type NEW struct{ base.Index16Instruction }
+
+func (self *NEW) Execute(frame *rtda.Frame) {
+	cp := frame.Method().Class().ConstantPool()
+	classRef := cp.GetConstant(self.Index).(*heap.ClassRef)
+	class := classRef.ResolvedClass()
+
+	if !class.InitStarted() {
+		frame.RevertNextPC()
+		base.InitClass(frame.Thread(), class)
+		return
+	}
+
+
+	if class.IsInterface() || class.IsAbstract() {
+		panic("java.lang.InstantiationError")
+	}
+
+	ref := class.NewObject()
+	frame.OperandStack().PushRef(ref)
+}
